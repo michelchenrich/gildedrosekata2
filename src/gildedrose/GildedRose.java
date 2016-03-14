77b47b2b8cdf373fccaaff276c504150605c9340
michelchenrich <michelchenrich@gmail.com>
2016-03-14 13:40:40 -0300

    simplifying conditionals (partial step)

--- src/gildedrose/GildedRose.java
+++ src/gildedrose/GildedRose.java
@@ -40,19 +40,17 @@ public class GildedRose {
             }
 
             if (item.sellIn < 0) {
-                if (!item.type.equals(Type.AGED)) {
-                    if (!item.type.equals(Type.TICKET)) {
-                        if (item.quality > 0) {
-                            if (!item.type.equals(Type.LEGENDARY)) {
-                                item.quality = item.quality - 1;
-                            }
-                        }
-                    } else {
-                        item.quality = item.quality - item.quality;
-                    }
-                } else {
+                if (item.type.equals(Type.AGED)) {
                     if (item.quality < 50) {
                         item.quality = item.quality + 1;
+                    }
+                } else {
+                    if (item.type.equals(Type.TICKET)) {
+                        item.quality = item.quality - item.quality;
+                    } else if (item.type.equals(Type.NORMAL)) {
+                        if (item.quality > 0) {
+                            item.quality = item.quality - 1;
+                        }
                     }
                 }
             }

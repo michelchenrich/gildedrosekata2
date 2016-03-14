2d8947e46ef55a4560be3d4781442c36a2e46a4e
michelchenrich <michelchenrich@gmail.com>
2016-03-14 13:23:42 -0300

    simplifying conditionals

--- src/gildedrose/GildedRose.java
+++ src/gildedrose/GildedRose.java
@@ -8,55 +8,53 @@ public class GildedRose {
     }
 
     public void updateQuality() {
-        for (int i = 0; i < items.length; i++) {
-            if (!items[i].type.equals(Type.AGED)
-                    && !items[i].type.equals(Type.TICKET)) {
-                if (items[i].quality > 0) {
-                    if (!items[i].type.equals(Type.LEGENDARY)) {
-                        items[i].quality = items[i].quality - 1;
-                    }
+        for (Item item : items) {
+            if (!item.type.equals(Type.AGED) && !item.type.equals(Type.TICKET)) {
+                if (item.quality > 0 && !item.type.equals(Type.LEGENDARY)) {
+                    item.quality = item.quality - 1;
                 }
             } else {
-                if (items[i].quality < 50) {
-                    items[i].quality = items[i].quality + 1;
+                if (item.quality < 50) {
+                    item.quality = item.quality + 1;
 
-                    if (items[i].type.equals(Type.TICKET)) {
-                        if (items[i].sellIn < 11) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
+                    if (item.type.equals(Type.TICKET)) {
+                        if (item.sellIn < 11) {
+                            if (item.quality < 50) {
+                                item.quality = item.quality + 1;
                             }
                         }
 
-                        if (items[i].sellIn < 6) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
+                        if (item.sellIn < 6) {
+                            if (item.quality < 50) {
+                                item.quality = item.quality + 1;
                             }
                         }
                     }
                 }
             }
 
-            if (!items[i].type.equals(Type.LEGENDARY)) {
-                items[i].sellIn = items[i].sellIn - 1;
+            if (!item.type.equals(Type.LEGENDARY)) {
+                item.sellIn = item.sellIn - 1;
             }
 
-            if (items[i].sellIn < 0) {
-                if (!items[i].type.equals(Type.AGED)) {
-                    if (!items[i].type.equals(Type.TICKET)) {
-                        if (items[i].quality > 0) {
-                            if (!items[i].type.equals(Type.LEGENDARY)) {
-                                items[i].quality = items[i].quality - 1;
+            if (item.sellIn < 0) {
+                if (!item.type.equals(Type.AGED)) {
+                    if (!item.type.equals(Type.TICKET)) {
+                        if (item.quality > 0) {
+                            if (!item.type.equals(Type.LEGENDARY)) {
+                                item.quality = item.quality - 1;
                             }
                         }
                     } else {
-                        items[i].quality = items[i].quality - items[i].quality;
+                        item.quality = item.quality - item.quality;
                     }
                 } else {
-                    if (items[i].quality < 50) {
-                        items[i].quality = items[i].quality + 1;
+                    if (item.quality < 50) {
+                        item.quality = item.quality + 1;
                     }
                 }
             }
         }
     }
+
 }

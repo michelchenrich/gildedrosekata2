fcf42a781c0a16a0bb24f3b09bbbdf0200db98d6
michelchenrich <michelchenrich@gmail.com>
2016-03-14 14:05:57 -0300

    added more cases to maxing quality at fifty

--- src/gildedrose/GildedRose.java
+++ src/gildedrose/GildedRose.java
@@ -22,14 +22,14 @@ public class GildedRose {
                 case TICKET: {
                     if (item.sellIn == 0)
                         item.quality = 0;
-                    else if (item.quality < 50) {
-                        if (item.sellIn > 10)
-                            item.quality += 1;
-                        else if (item.sellIn > 5)
-                            item.quality += 2;
-                        else
-                            item.quality += 3;
-                    }
+                    else if (item.sellIn > 10)
+                        item.quality += 1;
+                    else if (item.sellIn > 5)
+                        item.quality += 2;
+                    else
+                        item.quality += 3;
+                    if (item.quality > 50)
+                        item.quality = 50;
                     break;
                 }
                 case AGED: {

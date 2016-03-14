a8baa57e79536af9c0f4e38674c850da1db500fd
michelchenrich <michelchenrich@gmail.com>
2016-03-14 10:42:38 -0300

    test passed: normalDecreasesQualityByOne

--- test/gildedrose/GildedRoseTest.java
+++ test/gildedrose/GildedRoseTest.java
@@ -14,4 +14,12 @@ public class GildedRoseTest {
         assertEquals(0, items[0].sellIn);
     }
 
+    @Test
+    public void normalDecreasesQualityByOne(){
+        Item[] items = new Item[] { new Item("Normal", 1, 1)};
+        GildedRose shop = new GildedRose(items);
+        shop.updateQuality();
+        assertEquals(0, items[0].quality);
+    }
+
 }

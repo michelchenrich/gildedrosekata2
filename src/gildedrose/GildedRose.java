12f975be7361eaaa485d86239a1b4831011e09a0
michelchenrich <michelchenrich@gmail.com>
2016-03-14 13:47:04 -0300

    turned ifs to switch on type

--- src/gildedrose/GildedRose.java
+++ src/gildedrose/GildedRose.java
@@ -13,42 +13,49 @@ public class GildedRose {
                 item.sellIn = item.sellIn - 1;
             }
 
-            if (item.type.equals(Type.NORMAL)) {
-                if (item.quality > 0) {
-                    item.quality = item.quality - 1;
-                }
-                if (item.sellIn < 0) {
+            switch (item.type) {
+                case NORMAL: {
                     if (item.quality > 0) {
                         item.quality = item.quality - 1;
                     }
-                }
-            } else if (item.type.equals(Type.TICKET)) {
-                if (item.quality < 50) {
-                    item.quality = item.quality + 1;
-
-                    if (item.sellIn < 10) {
-                        if (item.quality < 50) {
-                            item.quality = item.quality + 1;
+                    if (item.sellIn < 0) {
+                        if (item.quality > 0) {
+                            item.quality = item.quality - 1;
                         }
                     }
+                    break;
+                }
+                case TICKET: {
+                    if (item.quality < 50) {
+                        item.quality = item.quality + 1;
 
-                    if (item.sellIn < 5) {
-                        if (item.quality < 50) {
-                            item.quality = item.quality + 1;
+                        if (item.sellIn < 10) {
+                            if (item.quality < 50) {
+                                item.quality = item.quality + 1;
+                            }
+                        }
+
+                        if (item.sellIn < 5) {
+                            if (item.quality < 50) {
+                                item.quality = item.quality + 1;
+                            }
                         }
                     }
+                    if (item.sellIn < 0) {
+                        item.quality = item.quality - item.quality;
+                    }
+                    break;
                 }
-                if (item.sellIn < 0) {
-                    item.quality = item.quality - item.quality;
-                }
-            } else if (item.type.equals(Type.AGED)) {
-                if (item.quality < 50) {
-                    item.quality = item.quality + 1;
-                }
-                if (item.sellIn < 0) {
+                case AGED: {
                     if (item.quality < 50) {
                         item.quality = item.quality + 1;
                     }
+                    if (item.sellIn < 0) {
+                        if (item.quality < 50) {
+                            item.quality = item.quality + 1;
+                        }
+                    }
+                    break;
                 }
             }
         }

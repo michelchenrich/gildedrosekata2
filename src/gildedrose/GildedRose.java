f4335d53eb2c3a70e4151a16219ffff31318f5e2
michelchenrich <michelchenrich@gmail.com>
2016-03-14 13:26:04 -0300

    simplifying conditionals (using same language as in specification)

--- src/gildedrose/GildedRose.java
+++ src/gildedrose/GildedRose.java
@@ -18,13 +18,13 @@ public class GildedRose {
                     item.quality = item.quality + 1;
 
                     if (item.type.equals(Type.TICKET)) {
-                        if (item.sellIn < 11) {
+                        if (item.sellIn <= 10) {
                             if (item.quality < 50) {
                                 item.quality = item.quality + 1;
                             }
                         }
 
-                        if (item.sellIn < 6) {
+                        if (item.sellIn <= 5) {
                             if (item.quality < 50) {
                                 item.quality = item.quality + 1;
                             }

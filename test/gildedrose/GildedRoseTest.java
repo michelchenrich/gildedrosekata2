b908c56f86288f799b3cd9d291fb6790c672b721
michelchenrich <michelchenrich@gmail.com>
2016-03-14 10:40:43 -0300

    test passed: normalItemAgesByOne

--- test/gildedrose/GildedRoseTest.java
+++ test/gildedrose/GildedRoseTest.java
@@ -7,11 +7,11 @@ import static org.junit.Assert.assertEquals;
 public class GildedRoseTest {
 
     @Test
-    public void foo() {
-        Item[] items = new Item[] { new Item("foo", 0, 0) };
-        GildedRose app = new GildedRose(items);
-        app.updateQuality();
-        assertEquals("fixme", app.items[0].name);
+    public void normalItemAgesByOne(){
+        Item[] items = new Item[] { new Item("Normal", 1, 1)};
+        GildedRose shop = new GildedRose(items);
+        shop.updateQuality();
+        assertEquals(0, items[0].sellIn);
     }
 
 }
